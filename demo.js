fba2823e8a2491d94a5fd9e6cb31b1494f9546c4
HarishYuvaraj <harishyuvaraj3@gmail.com>
2024-09-04 23:50:41 -0700

    I want to share a code

---
+++ demo.js
@@ -0,0 +1,8 @@
+var sidenavbar=document.querySelector(".side-navbar")
+var navbarmenutoggle=document.querySelector(".navbar-menu-toggle")
+function shownavbar(event){
+    sidenavbar.style.left="0"
+}
+function closenavbar(){
+    sidenavbar.style.left="-60%"
+}
